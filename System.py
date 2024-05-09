a31e3490c34843fad6906bf06f9457a0c1e2675b
Pharalone <kolim19yt@gmail.com>
2024-05-09 20:52:41 +0300

    update System.py, console.py

--- System.py
+++ System.py
@@ -3,19 +3,63 @@ import os
 import urllib.request
 url = 'https://raw.githubusercontent.com/Xuvel/Python-OS-/main/OS_VERSION.txt'
 
+null = "null"
 
 github_verfile = "OS_VERSION.txt"
 
 systemVer      = "0.0.1"
 systemName     = "AAA Python OS"
 
-def get_ver():
+
+class User:
+    def __init__(self, name, permissions, *password):
+        self.name        = name
+        self.permissions = permissions
+        self.password    = password
+
+    def changeName(self, newName, *password) -> int:
+        if (newName != ""):
+            if (self.password != ""):
+                if (self.password == password):
+                    self.name = newName
+                    return 0x0 # successfull
+                else:
+                    return 0x1 # incorrect password
+            else:
+                self.name = newName
+                return 0x0     # successfull
+        else:
+            return 0x2         # new name empty
+
+                
+    def setPassword(self, newPassword, *current_password) -> int:
+        if (not self.password):
+            if (current_password == self.password):
+                self.password = newPassword
+                return 0x0                                          # successful
+            else:
+                return 0x1                                          # incorrect password
+        else:
+            self.password = newPassword
+            return 0x0                                              # new password
+                                               # successful
+    
+    def createUser(name, permission, *password) -> int:
+
+        if (password != ""):
+            return User(name, permission)
+        else:
+            return User(name, permission, password)
+        
+
+
+def getVersion() -> str:
     return systemVer
 
-def get_name():
+def getName() -> str:
     return systemName
 
-def get_last_version():
+def getLastVersion() -> str:
     try:
         filename = github_verfile
         urllib.request.urlretrieve(url, filename)
@@ -23,8 +67,8 @@ def get_last_version():
         fileContent = file.read()
         return fileContent
     except:
-        return "null"
-        
+        return null
+    
 
 
-print(get_last_version())
+print(getLastVersion())
--- console.py
+++ console.py
@@ -5,7 +5,7 @@ import subprocess
 import System
 
 def mainDir():
-	return ""
+	return os.getcwd()
 
 if (os.name == "nt"):
 	
@@ -14,30 +14,57 @@ else:
 	clear = lambda: os.system('clear')
 command_list = ["help", "stop"]
 version = "v.0.1"
-stop = False
+isRunning = True
 user_input = ""
-error_level = 0x00000
+error_level = 0x0
 
 def check(command):
 	
-	global stop
+	global isRunning
 
-	print(command[7:15])
+	print(command[7:14])
 
-	if (command == "stop"):
-		stop = True
+	if (command == "exit"):
+		isRunning = False
 
 	if (len(command) >= 2):
 		if (command[:2] == "do"):
 			
 			if (command[3:7] == "get-"):
 
-				if (command[7:19] == "-currentVer"):
-					print(System.get_last_version())
+				if (command[7:20] == "-current_ver"):
+					print(System.getLastVersion())
 				elif (command[7:15] == "-os_ver"):
-					print(System.get_ver())
+					print(System.getVersion())
 				elif (command[7:16] == "-os_name"):
-					print(System.get_name())
+					print(System.getName())
+				elif (command[7:18] == "-user_name"):
+					print()
+				elif (command[7:11] == "-os"):
+					print(System.getOs())
+			
+			elif (command[3:10] == "create-"):
+				if (command[10:15] == "-user"):
+					name = input("type a name: ")
+					if (name != ""):
+						permissions = input("type a type of permission (ADM or USER): ")
+						if (permissions == "ADM" or permissions == "USER"):
+							password = input("type a password: ")
+							if (password != ""):
+								return None
+								# eval(name) = System.User.createUser(name, permissions, password)
+							else:
+								return None
+								# newUser = System.User.createUser(name, permissions)
+						else:
+							print("incorrect permission type")
+					else:
+						print("name is empty")
+
+			elif (command[3:7] == "set-"):
+				if (command[7:14] == "-name"):
+
+					print("successful") if (newUser.changeName(command[14:]) == 0x0) else print("incorrect password") if (newUser.changeName(command[14:]) == 0x1) else print("unknown")
 
 
 
@@ -45,8 +72,8 @@ print(f"Triple A DOS {version}")
 def main():
 	time.sleep(0)
 	print("Triple A OS console")
-	while (stop == False):
-		print(">>", end = "")
+	while (isRunning):
+		print(f"{mainDir()} $ ", end = "")
 		user_input = input()
 		check(user_input)
 	return error_level
