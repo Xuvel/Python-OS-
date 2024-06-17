52a95da9d1fbaa6a77852e5fcfdd3cc79c1e4bbe
unknown <aleks161208@gmail.com>
2024-06-17 12:36:28 +0300

    Update bios.py to 1.0.3 beta and testing programms

--- console.py
+++ console.py
@@ -8,101 +8,62 @@ def mainDir():
 	return os.getcwd()
 
 if (os.name == "nt"):
-	
 	clear = lambda: os.system('cls')
 else:
 	clear = lambda: os.system('clear')
+
 command_list = ["help", "stop"]
 version = "v.0.1"
-isRunning = True
+is_running = True
 user_input = ""
 error_level = 0x0
+cum_times = 0
 
-def loading(times, text, wait):
-	for i in range(times):
-		print(f"loading [|]{text}")
-		time.sleep(wait)
-		clear()
-		print(f"loading [/]{text}")
-		time.sleep(wait)
-		clear()
-		print(f"loading [-]{text}")
-		time.sleep(wait)
-		clear()
-		print(f"loading [\\]{text}")
-		time.sleep(wait)
-		clear()
+history = []
 
-def check(command):
-	
-	global isRunning
+def s(command): # блять повесся нахуй с своими ебанными НАЗВАНИЯМИ БЛЯТЬ ИДИ НАХУЙ ЧМОШН ИК ЕБАННЫЙ АААААААААААААААААААААААААААААААААААААААААААААААААААААААААААВРДЖПОЬЬЬЬЬЬЬЬЦКрЖЭЬ
+	history.append(command)
+	match command:
+		case "help":
+			print(
+				"""
+				Список команд:
+				help - показать список команд
+				whoami - вывести текущего пользователя
+				echo [текст] - вывести текст в консоль
+				history - вывести историю команд
+				cum - ну вы поняли...
+				exit - выход
+				""")
+			pass
+		case "whoami":
+			pass # Вывод текущего юзера
+		case "echo":
+			if (len(command) > 4):
+				print(command[4:])
+		case "history":
+			for index, item in enumerate(history):
+				print(f"{index + 1} {item}")
 
-	print(command[7:14])
-
-	if (command == "exit"):
-		isRunning = False
-
-	if (len(command) >= 2):
-		if (command[:2] == "do"):
+		case "exit":
+			error_level = 1
+			is_running = False
+			pass
+		case "cum":
+			if (cum_times <= System.maxCums):
+				print("cummed!")
+				cum_times += 1
+			else:
+				print(f"You can't cum more {System.maxCums}")	
+		case _:
+			print(f"{command} не является действительной командой")
 			
-			if (command[3:7] == "get-"):
 
-				if (command[7:20] == "-current_ver"):
-					print(System.getLastVersion())
-				elif (command[7:15] == "-os_ver"):
-					print(System.getVersion())
-				elif (command[7:16] == "-os_name"):
-					print(System.getName())
-				elif (command[7:18] == "-user_name"):
-					print()
-				elif (command[7:11] == "-os"):
-					print(System.getOs())
-			
-			elif (command[3:10] == "create-"):
-				if (command[10:15] == "-user"):
-					name = input("type a name: ")
-					if (name != ""):
-						permissions = input("type a type of permission (ADM or USER): ")
-						if (permissions == "ADM" or permissions == "USER"):
-							password = input("type a password: ")
-							newUser = System.User.createUser(name, permissions, password)
-							System.users.append(newUser)
-						else:
-							print("incorrect permission type")
-					else:
-						print("name is empty")
-
-			elif (command[3:7] == "set-"):
-				if (command[7:14] == "-name"):
-
-					print("successful") if (newUser.changeName(command[14:]) == 0x0) else print("incorrect password") if (newUser.changeName(command[14:]) == 0x1) else print("unknown")
-
-
-
-print(f"Triple A DOS {version}")
 def main():
-	time.sleep(0)
-	loading(4, "", 0)
-	print("Triple A OS console")
-
-	userName = input("Enter username")
-	password = input("Enter password")
-
-	if (System.users.count != 0):
-		System.User.createUser(userName, "ADM", password)
-	elif (userName in System.users):
-		
-		if (System.users[System.users.find(userName)].getPassword() == password):
+	while (is_running):
+		user_input = input()
 
 
-			while (isRunning):
-				print(f"{mainDir()} $ ", end = "")
-				user_input = input()
-				check(user_input)
-			return error_level
-		else:
-			return 0x1
-	else:
-		return 0x2
-	
-print(f"Program finished with exit code {hex(main())}")
+
+
+print(f"Program finished with exit code {main()}")
